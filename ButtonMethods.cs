272859df0fae7933bb3444355b68dd5f4418b269
CurtisSquire <45086802+CurtisSquire@users.noreply.github.com>
2019-03-09 17:53:50 -0500

    Update ButtonMethods.cs

--- ButtonMethods.cs
+++ ButtonMethods.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement; 
-
+//this is where I create all methods for the buttons in my game. 
 public class ButtonMethods : MonoBehaviour
 {
     public void quit()
